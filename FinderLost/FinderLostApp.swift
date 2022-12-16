127960e3542971d068f6495e2c11189b572b6b4b
Andrés Felipe <aalzres@gmail.com>
2022-12-16 21:05:33 +0100

    Initial commit

---
+++ FinderLost/FinderLostApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderLostApp.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 16/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FinderLostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
