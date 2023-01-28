ff032f1d6c7a8b38a10705af934482c4e0c1e813
Andrés Felipe <aalzres@gmail.com>
2023-01-28 15:27:41 +0000

    Implementing SwiftUIViewLyfeCycle

---
+++ FinderLost/Architecture/SwiftUIViewLyfeCycle.swift
@@ -0,0 +1,12 @@
+//
+//  SwiftUIViewLyfeCycle.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 28/1/23.
+//
+
+// [SwiftUI View LyfeCycle](https://www.vadimbulavin.com/swiftui-view-lifecycle/)
+public enum SwiftUIViewLyfeCycle {
+    case onAppear
+    case onDisappear
+}
