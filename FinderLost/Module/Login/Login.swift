b30d72020d49a8ae532a51feb2fd516b97c272bc
Andrés Felipe <aalzres@gmail.com>
2023-02-06 21:44:16 +0000

    Refactor to Login State and Action

--- FinderLost/Module/Login/Login.swift
+++ FinderLost/Module/Login/Login.swift
@@ -5,20 +5,13 @@
 //  Created by Andres Felipe Alzate Restrepo on 31/1/23.
 //
 
-// MARK: - Module Login
 enum Login {
     struct State {
         var userId: String = ""
-        var tokenSession: String = ""
         var error: String = ""
-        var submodule: Submodule.State = .init()
     }
-    enum ResultAction {
-        case checkSession(Result<String, Error>)
-    }
-    enum Action {
-        case result(ResultAction)
+    enum Action: ReduxAction {
         case checkSession
-        case submodule(Submodule.Action)
+        case checkSessionResult(Result<String, Error>)
     }
 }
