4dee625a72a7c4f9f38d4508e4b090bcf4835845
Andrés Felipe <aalzres@gmail.com>
2023-02-06 21:41:17 +0000

    Refactor to Login Reducer

--- FinderLost/Module/Login/LoginReducer.swift
+++ FinderLost/Module/Login/LoginReducer.swift
@@ -6,18 +6,14 @@
 //
 
 // MARK: - Login Reducer
-extension Login: ReducerBase {
+extension Login: Reducer {
     static func reduce(_ action: Action, with state: State) -> State {
         var newState = state
         switch action {
-        case let .result(.checkSession(.success(value))):
+        case let .checkSessionResult(.success(value)):
             newState.userId = value
-        case let .result(.checkSession(.failure(error))):
+        case let .checkSessionResult(.failure(error)):
             newState.error = error.localizedDescription
-        case let .submodule(.checkSubmodule(value)):
-            newState.error = value
-        case let .submodule(action):
-            newState.submodule = Submodule.reduce(action, with: state.submodule)
         default: break
         }
         return newState
