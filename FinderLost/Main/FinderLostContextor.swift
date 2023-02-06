a40eadfd630dfdc84ecccd95ff62a22e7f724b8c
Andrés Felipe <aalzres@gmail.com>
2023-02-06 21:46:34 +0000

    Refactor to FinderLost Handler and rename to Contextor

---
+++ FinderLost/Main/FinderLostContextor.swift
@@ -0,0 +1,15 @@
+//
+//  FinderLostContextor.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 31/1/23.
+//
+
+extension FinderLost: Contextor {
+    static func context(_ action: Action, with state: State) -> any HandlerContext {
+        switch action {
+        case let .login(action):
+            return HandlerContextImpl(action: action, state: state.login, next: Action.login)
+        }
+    }
+}
