581635547b7d1117659177ded6693e9f2e213606
Andrés Felipe <aalzres@gmail.com>
2023-02-06 21:44:59 +0000

    Refactor to Login Reducer

--- FinderLost/Main/FinderLostReducer.swift
+++ FinderLost/Main/FinderLostReducer.swift
@@ -5,13 +5,11 @@
 //  Created by Andres Felipe Alzate Restrepo on 31/1/23.
 //
 
-// MARK: - FinderLost Reducer
-extension FinderLost: ReducerBase {
+extension FinderLost: Reducer {
     static func reduce(_ action: Action, with state: State) -> State {
         var newState = state
         switch action {
-        case let .login(action):
-            newState.login = Login.reduce(action, with: state.login)
+        case let .login(action): newState.login = Login.reduce(action, with: state.login)
         }
         return newState
     }
