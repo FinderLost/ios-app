54a4dcc90a8a3b067ed2c9450c3a9c7459721149
Andrés Felipe <aalzres@gmail.com>
2023-02-06 21:52:36 +0000

    Implemented some tool to cast types

---
+++ FinderLost/InternalFramework/Publisher+.swift
@@ -0,0 +1,14 @@
+//
+//  Publisher+.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 6/2/23.
+//
+
+import Combine
+
+extension Publisher {
+    public func mapCast<T>(_ type: T.Type) -> AnyPublisher<T, Self.Failure> {
+        compactMap { $0 as? T }.eraseToAnyPublisher()
+    }
+}
