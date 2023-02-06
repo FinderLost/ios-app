4f1f3451e2b5e474863d61abd8eed5bb5a5cabca
Andrés Felipe <aalzres@gmail.com>
2023-02-06 21:52:02 +0000

    Implemented contextor, refactor to handler, implement HandlerContext, implemented redux action, reducer and redux

---
+++ FinderLost/Architecture/Redux/Redux.swift
@@ -0,0 +1,12 @@
+//
+//  Redux.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 31/1/23.
+//
+
+protocol Redux {
+    associatedtype Action
+    associatedtype State
+}
+
---
+++ FinderLost/Architecture/Redux/ReduxAction.swift
@@ -0,0 +1,8 @@
+//
+//  ReduxAction.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 6/2/23.
+//
+
+protocol ReduxAction { }
