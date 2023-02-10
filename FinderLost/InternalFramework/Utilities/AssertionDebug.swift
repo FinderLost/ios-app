da893a140ee7e4e5bea21f8f5366dc1261506da3
Andrés Felipe <aalzres@gmail.com>
2023-02-10 19:38:17 +0000

    Implemented AssertionDebug

---
+++ FinderLost/InternalFramework/Utilities/AssertionDebug.swift
@@ -0,0 +1,15 @@
+//
+//  AssertionDebug.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 10/2/23.
+//
+
+@inlinable
+public func assertionDebug(
+    _ message: @autoclosure () -> String = String(),
+    file: StaticString = #file,
+    line: UInt = #line
+) {
+    assertionFailure(message(), file: file, line: line)
+}
