8bc6bff33b9fe961eea8a2ef4042c146cfc987aa
Andrés Felipe <aalzres@gmail.com>
2023-02-10 19:40:27 +0000

    Comment map cast

--- FinderLost/InternalFramework/Publisher+.swift
+++ FinderLost/InternalFramework/Publisher+.swift
@@ -8,7 +8,18 @@
 import Combine
 
 extension Publisher {
+    /// Transforms elements in the publisher to the specified type, excluding elements that cannot be cast to that type.
+    ///
+    /// - Parameter type: The type to cast elements to.
+    /// - Returns: A new publisher of the specified type, excluding elements that could not be cast.
     func mapCast<T>(_ type: T.Type) -> AnyPublisher<T, Self.Failure> {
-        compactMap { $0 as? T }.eraseToAnyPublisher()
+        compactMap {
+            guard let newType = $0 as? T else {
+                assertionDebug("Failed to cast element to specified type")
+                return nil
+            }
+            return newType
+        }
+        .eraseToAnyPublisher()
     }
 }
