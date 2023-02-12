4c06b29e93e11706cf820f5ec72da05cd32c66f9
Andrés Felipe <aalzres@gmail.com>
2023-02-12 00:39:49 +0000

    new entity user info

---
+++ Packages/Domain/Sources/Domain/Entity/UserInfo.swift
@@ -0,0 +1,11 @@
+//
+//  UserInfo.swift
+//  
+//
+//  Created by Andres Felipe Alzate Restrepo on 11/2/23.
+//
+
+public protocol UserInfo {
+    var name: String { get }
+    var email: String { get }
+}
