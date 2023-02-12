dedadef8045497294066f33218cb514727a7df74
Andrés Felipe <aalzres@gmail.com>
2023-02-12 00:44:08 +0000

    Implemented the design system with some logic for the color

---
+++ Packages/DesignSystem/Sources/DesignSystem/ColorPalette/ColorPalette.swift
@@ -0,0 +1,19 @@
+//
+//  ColorPalette.swift
+//  
+//
+//  Created by Andres Felipe Alzate Restrepo on 11/2/23.
+//
+
+public enum ColorPalette: String, CaseIterable {
+    case background
+    case dark
+    case error
+    case first
+    case information
+    case light
+    case second
+    case success
+    case text
+    case warning
+}
