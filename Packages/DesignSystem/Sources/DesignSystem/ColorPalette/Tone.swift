0f37a4de9a2741694256467ce6f8edced914e005
Andrés Felipe <aalzres@gmail.com>
2023-02-12 00:44:08 +0000

    Implemented the design system with some logic for the color

---
+++ Packages/DesignSystem/Sources/DesignSystem/ColorPalette/Tone.swift
@@ -0,0 +1,15 @@
+//
+//  Tone.swift
+//  
+//
+//  Created by Andres Felipe Alzate Restrepo on 11/2/23.
+//
+
+public enum Tone: String {
+    case tone10
+    case tone25
+    case tone30
+    case tone75
+    case dark
+    case light
+}
