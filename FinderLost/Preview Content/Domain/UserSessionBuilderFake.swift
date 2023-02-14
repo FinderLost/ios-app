5c6d20832b5d61298050984d404af92d7e1bf012
Andrés Felipe <aalzres@gmail.com>
2023-02-14 20:30:35 +0000

    Preparing mocks and cleaning code

---
+++ FinderLost/Preview Content/Domain/UserSessionBuilderFake.swift
@@ -0,0 +1,25 @@
+//
+//  UserSessionBuilderFake.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 14/2/23.
+//
+
+import Domain
+import Utilities
+
+struct UserSessionBuilderFake: Modifiable {
+    var token: String = "abc123"
+    var userId: String = "user123"
+
+    var entity: UserSession {
+        UserSessionImpl(
+            token: token,
+            userId: userId
+        )
+    }
+    private struct UserSessionImpl: UserSession {
+        let token: String
+        let userId: String
+    }
+}
--- FinderLost/Preview Content/Login/SignInBuilderFake.swift
+++ FinderLost/Preview Content/Login/SignInBuilderFake.swift
@@ -11,14 +11,12 @@ import Foundation
 
 extension Login {
     struct SignInBuilderFake: Modifiable {
-        var userId: String = ""
-        var name: String = ""
-        var email: String = ""
+        var name: String = "test@email.com"
+        var email: String = "Test User"
         var imageUrl: URL? = URL(string: "https://cdn.icon-icons.com/icons2/2468/PNG/512/user_kids_avatar_user_profile_icon_149314.png")
 
         var entity: Login.SignIn {
             .init(
-                userId: userId,
                 email: email,
                 name: name,
                 imageUrl: imageUrl
