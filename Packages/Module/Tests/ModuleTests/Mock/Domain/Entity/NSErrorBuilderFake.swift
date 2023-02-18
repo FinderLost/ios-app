5c7cabd63444920916f3b47a78cfa79243b67d91
Andrés Felipe <aalzres@gmail.com>
2023-02-18 00:05:55 +0000

    Builder fake to Module

---
+++ Packages/Module/Tests/ModuleTests/Mock/Domain/Entity/NSErrorBuilderFake.swift
@@ -0,0 +1,24 @@
+//
+//  NSErrorBuilderFake.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 14/2/23.
+//
+
+import Utilities
+
+import Foundation
+
+struct NSErrorBuilderFake: Modifiable {
+    var domain: String = "Test"
+    var code: Int = 0
+    var userInfo: [String: Any]? = nil
+
+    var entity: NSError {
+        .init(
+            domain: domain,
+            code: code,
+            userInfo: userInfo
+        )
+    }
+}
