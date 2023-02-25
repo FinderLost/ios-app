3b479df2c84bb4cb3aa1884f8b0cc61f7a7f42b3
Andrés Felipe <aalzres@gmail.com>
2023-02-25 10:19:30 +0000

    YourMissingView implemented

---
+++ FinderLost/App/YourMissingView.swift
@@ -0,0 +1,50 @@
+//
+//  YourMissingView.swift
+//  FinderLost
+//
+//  Created by Andres Felipe Alzate Restrepo on 22/1/23.
+//
+
+import Redux
+
+import Factory
+import SwiftUI
+
+struct YourMissingView: View {
+    @EnvironmentObject
+    var store: Store<FinderLost>
+
+    var body: some View {
+        NavigationView {
+            if let privateList = store.state.missing.hasData?.privateList {
+                List(privateList, id: \.self) { item in
+                    Text(item)
+                }
+            } else {
+                ZStack {
+                    Text("no missing yet")
+                }
+            }
+        }
+        .isLoading(store.state.missing.isLoading)
+        .navigationBarTitle("Your missing")
+        .navigationBarItems(trailing: Button(action: { }) { Image(systemName: "plus") } )
+        .background(Color.background)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider, PreviewContent {
+    static var previews: some View {
+        let store = storeBuilderFake(
+            initialState: .init()
+        )
+
+        TabView {
+            NavigationView { YourMissingView() }
+                .tabItem { Image(systemName: "square.split.2x2.fill") }
+        }
+        .environmentObject(store)
+        .previewLayout(PreviewLayout.sizeThatFits)
+        .previewDisplayName("Default preview")
+    }
+}
