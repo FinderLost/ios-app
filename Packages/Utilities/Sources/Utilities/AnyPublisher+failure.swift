7b896a58180e4e39abfd817d55fb40c165a11836
Andrés Felipe <aalzres@gmail.com>
2023-03-04 22:52:46 +0000

    implementing documentation

--- Packages/Utilities/Sources/Utilities/AnyPublisher+failure.swift
+++ Packages/Utilities/Sources/Utilities/AnyPublisher+failure.swift
@@ -8,8 +8,12 @@
 import Combine
 
 extension AnyPublisher {
+    ///
+    /// Creates a publisher that immediately terminates with the specified error.
+    ///
+    ///- Parameter error: The error to send when the publisher terminates.
+    ///- Returns: A publisher that immediately terminates with the specified error.
     public static func failure<Value, Error>(_ error: Error) -> AnyPublisher<Value, Error> {
-        Fail(error: error)
-            .eraseToAnyPublisher()
+        Fail(error: error).eraseToAnyPublisher()
     }
 }
