483759c77e45ecd2fc0bc170322be478f7f1f65b
Andrés Felipe <aalzres@gmail.com>
2023-03-04 09:10:14 +0000

    Add missing implementation and response

---
+++ Packages/Domain/Sources/Domain/Entity/Missing.swift
@@ -0,0 +1,11 @@
+//
+//  Missing.swift
+//  
+//
+//  Created by Andres Felipe Alzate Restrepo on 3/3/23.
+//
+
+public protocol Missing {
+    var name: String { get }
+    var description: String { get }
+}
