8f5922302654728de94dd78f3c9fe290441f8fe1
Andrés Felipe <aalzres@gmail.com>
2023-03-04 12:07:46 +0000

    code comment

--- Packages/Utilities/Sources/Utilities/AnyPublisher+compactMapMany.swift
+++ Packages/Utilities/Sources/Utilities/AnyPublisher+compactMapMany.swift
@@ -8,12 +8,10 @@
 import Combine
 
 extension AnyPublisher where Output: Collection {
-    /**
-     Projects each element of an observable collection into a new form.
-
-     - parameter transform: A transform function to apply to each element of the source collection.
-     - returns: An observable collection whose elements are the result of invoking the transform function on each element of source.
-     */
+    /// An extension to compact map elements of a collection into a new form that can be optionally nil.
+    ///
+    /// - parameter transform: A closure that takes an element of the collection as its argument and returns an optional value.
+    /// - returns: A publisher that contains an array of the non-nil results of calling the transform
     public func compactMapMany<Result>(_ transform: @escaping (Output.Element) -> Result?) -> AnyPublisher<[Result], Failure> {
         map { collection -> [Result] in
             collection.compactMap(transform)
