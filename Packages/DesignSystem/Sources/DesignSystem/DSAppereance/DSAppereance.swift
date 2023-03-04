992d9cc5ef399b36fa2359f612643d4d7be61c17
Andrés Felipe <aalzres@gmail.com>
2023-03-04 16:52:34 +0000

    DSAppereance implemented

---
+++ Packages/DesignSystem/Sources/DesignSystem/DSAppereance/DSAppereance.swift
@@ -0,0 +1,14 @@
+//
+//  DSAppereance.swift
+//  
+//
+//  Created by Andres Felipe Alzate Restrepo on 4/3/23.
+//
+
+import UIKit
+
+enum DSAppereance {
+    public static func setAppearance() {
+        UIView.setAppearance()
+    }
+}
---
+++ Packages/DesignSystem/Sources/DesignSystem/DSAppereance/UIView+setAppearance.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+setAppearance.swift
+//  
+//
+//  Created by Andres Felipe Alzate Restrepo on 4/3/23.
+//
+
+import SwiftUI
+
+extension UIView {
+    static func setAppearance() {
+        let view = UIView.appearance()
+        view.tintColor = UIColor(Color.dsPrimary)
+    }
+}
