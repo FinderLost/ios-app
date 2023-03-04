438bfad4eb5d02a3cec56acbc3dff29ebc2d803f
Andrés Felipe <aalzres@gmail.com>
2023-03-04 16:53:14 +0000

    DSList and DSListItem

---
+++ Packages/DesignSystem/Sources/DesignSystem/List/DSListItem.swift
@@ -0,0 +1,18 @@
+//
+//  DSListItem.swift
+//  
+//
+//  Created by Andres Felipe Alzate Restrepo on 4/3/23.
+//
+
+import SwiftUI
+
+public struct DSListItem: ViewModifier {
+    public init() {}
+    public func body(content: Content) -> some View {
+        content
+            .listRowSeparator(.hidden)
+            .listRowBackground(Color.clear)
+            .background(Color.specific(.surface))
+    }
+}
