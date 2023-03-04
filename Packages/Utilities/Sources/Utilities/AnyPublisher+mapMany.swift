a4eabdfa22eebf7efdeb90600244703c99f18587
Andrés Felipe <aalzres@gmail.com>
2023-03-04 09:10:37 +0000

    adding tools for any publisher

---
+++ Packages/Utilities/Sources/Utilities/AnyPublisher+mapMany.swift
@@ -0,0 +1,23 @@
+//
+//  AnyPublisher+mapMany.swift
+//  
+//
+//  Created by Andres Felipe Alzate Restrepo on 3/3/23.
+//
+
+import Combine
+
+extension AnyPublisher where Output: Collection {
+    /**
+     Projects each element of an observable collection into a new form.
+
+     - parameter transform: A transform function to apply to each element of the source collection.
+     - returns: An observable collection whose elements are the result of invoking the transform function on each element of source.
+     */
+    public func mapMany<Result>(_ transform: @escaping (Output.Element) -> Result) -> AnyPublisher<[Result], Failure> {
+        map { collection -> [Result] in
+            collection.map(transform)
+        }
+        .eraseToAnyPublisher()
+    }
+}
