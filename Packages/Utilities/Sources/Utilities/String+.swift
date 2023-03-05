415b356e318ade80013b1e4c882ce555176d80e6
Andrés Felipe <aalzres@gmail.com>
2023-03-05 16:59:36 +0000

    Adding comment to the code

--- Packages/Utilities/Sources/Utilities/String+.swift
+++ Packages/Utilities/Sources/Utilities/String+.swift
@@ -6,5 +6,10 @@
 //
 
 extension String {
+    ///
+    /// An extension to the `String` type that provides an easy way to check if a string is not empty.
+    ///
+    /// Use `isNotEmpty` property to check if the string is not empty. It returns true if the string is not empty, false otherwise.
+    ///
     public var isNotEmpty: Bool { isEmpty.not }
 }
