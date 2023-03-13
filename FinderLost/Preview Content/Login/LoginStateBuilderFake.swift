3f8a7f4f8ea90fd091786f162ee0661f5334473c
Andrés Felipe <aalzres@gmail.com>
2023-03-13 07:54:56 +0000

    Moving the builder fakes

--- FinderLost/Preview Content/Login/LoginStateBuilderFake.swift
+++ FinderLost/Preview Content/Login/LoginStateBuilderFake.swift
@@ -5,20 +5,21 @@
 //  Created by Andres Felipe Alzate Restrepo on 18/2/23.
 //
 
+import Module
 import Utilities
 
 import Foundation
 
 extension BuilderFake {
-    public enum Login {
-        public struct Data: Modifiable {
+    enum LoginTCA {
+        struct Data: Modifiable {
             var email: String = "Test User"
             var name: String = "test@email.com"
             var imageUrl: URL? = URL(
                 string: "https://cdn.icon-icons.com/icons2/2468/PNG/512/user_kids_avatar_user_profile_icon_149314.png"
             )
-            public init() {}
-            public var entity: Module.LoginTCA.Data {
+            init() {}
+            var entity: Module.LoginTCA.Data {
                 .init(
                     email: email,
                     name: name,
--- Packages/Module/Sources/Module/Missing/MissingTCA.swift
+++ Packages/Module/Sources/Module/Missing/MissingTCA.swift
@@ -16,9 +16,21 @@ public enum MissingTCA {
         public var id: UUID = UUID()
         public let name: String
         public let description: String
+        public init(
+            id: UUID = UUID(),
+            name: String,
+            description: String
+        ) {
+            self.id = id
+            self.name = name
+            self.description = description
+        }
     }
     public struct Data: ReduxState {
         public let publicList: [DataItem]
+        public init(publicList: [MissingTCA.DataItem]) {
+            self.publicList = publicList
+        }
     }
     public enum State: ReduxState {
         case idle
